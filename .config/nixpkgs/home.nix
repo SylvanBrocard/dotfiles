7fdbb87d4e80d6c796fb3fc9a4978e62762c1d00
SylvanBrocard <sylvan.brocard@gmail.com>
2022-04-08 11:12:12 +0200

    added vscode firefox emacs

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -1,6 +1,5 @@
 { config, pkgs, lib, ... }:
 
-
 {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
@@ -23,11 +22,12 @@
   # Set variables for non-NixOS Linux.
   targets.genericLinux.enable = true;
 
+  nixpkgs.config.allowUnfree = true;
+
   fonts.fontconfig.enable = true;
 
   home.packages = with pkgs;[
      source-code-pro
-     emacs
      zotero
      micromamba
   ];
@@ -86,5 +86,27 @@
     plugins = with pkgs; [
       tmuxPlugins.nord
     ];
+  };
+
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    extensions = with pkgs.vscode-extensions; [
+      github.copilot
+      ms-python.python
+      eamodio.gitlens
+      bbenoist.nix
+      ms-azuretools.vscode-docker
+      ms-vscode-remote.remote-containers
+      davidanson.vscode-markdownlint
+    ];
+  };
+
+  programs.emacs = {
+    enable = true;
+  };
+
+  programs.firefox = {
+    enable = true;
   };
 }
